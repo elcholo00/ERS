7d1dab98ab8f5cede4b67fe4c967d3166a33eafe
elcholo01 <dvdcolak@gmail.com>
2023-12-15 13:40:19 +0100

    Projektovanje resenja i implementacija Modela projekta

---
+++ Projekat/Baza/BazaImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza
+{
+    public class BazaImpl : IBaza
+    {
+       public BazaImpl instance { get; set; }=null;
+
+        public BazaImpl() 
+        { 
+        }
+
+
+        public BazaImpl getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new BazaImpl();
+            }
+               
+            return instance;
+
+        }
+    }
+}

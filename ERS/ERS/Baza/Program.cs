10ed2252debe97c12a5165389fb05b16617155e0
elcholo01 <dvdcolak@gmail.com>
2023-12-23 12:34:44 +0100

    Implementacija baze

---
+++ ERS/ERS/Baza/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baza.Model;
+
+namespace Baza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BazaImpl baza = BazaImpl.GetBaza();
+
+            Audit a=new Audit(baza.getNextAuditId(),new DateTime(2022,10,10),"ajkgsd","SRB",10);
+
+
+            baza.InsertAudit(a);
+           // baza.DeleteAudit(a.Id);
+
+
+
+        }
+    }
+}
